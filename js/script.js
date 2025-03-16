267e0a85bad56380ace153f09ee6c09e82df553e
Sakib Hasan <sakibhasan95726@gmail.com>
2025-03-16 17:46:58 +0600

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+let counter = document.querySelectorAll('.counter');
+
+let arr = Array.from(counter);
+
+
+  arr.map(item => {
+    let counterNumber = item.innerHTML;
+
+    function counterjs() {
+      counterNumber--;
+      item.innerHTML = counterNumber;
+      if (counterNumber == 0) {
+        clearInterval(stop);
+      }
+    }
+
+    let stop = setInterval(() => {
+      counterjs();
+    }, 400);
+  });
+
